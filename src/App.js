afd3ab56c1c895dd5bb06e597710ae98ce2b56f7
gohomewho <ga676005@hotmail.com.tw>
2022-04-09 20:02:27 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import CharacterEditor from './components/CharacterEditor'
+import Footer from './components/Footer'
+import Block from './components/Block'
+
+function App () {
+  return (
+    <>
+      <Block />
+      <CharacterEditor />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Block/Block.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import styles from './Block.module.css'
+
+const Block = () => {
+  return <div className={styles.block}></div>
+}
+
+export default Block
